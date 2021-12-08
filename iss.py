96e4609d1aa5988ca81ec2f1e0c2cfba29bc9467
jakubKuznik <kuznja16@gmail.com>
2021-12-08 14:11:51 +0100

    python script created

---
+++ iss.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Kuznik Jakub
+# xkuzni04@stud.fit.vutbr.cz
+
+import time
+import sys
+
+def main():
+    print("hello")
+
+
+
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    print(time.time() - start)
